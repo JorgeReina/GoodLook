c92a5850c37e7378050e8c025c73c4c65698dff6
Jorge Reina <reinaromerojorge@gmail.com>
2024-04-28 17:46:53 +0200

    Plantillas Sign-In y Sign-Up

--- FrontEndGoodLook/src/app/app-routing.module.ts
+++ FrontEndGoodLook/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component'
+import { Page404Component } from './page404/page404.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   { path: 'inicio', component: BodyComponent},
+  { path: 'sign-up', component: SignupComponent},
+  { path: 'sign-in', component: SigninComponent},
   { path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: '**', component: Page404Component},
 ];
 
 @NgModule({
--- FrontEndGoodLook/src/app/app.module.ts
+++ FrontEndGoodLook/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
+import { Page404Component } from './page404/page404.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { NavComponent } from './nav/nav.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    Page404Component,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
