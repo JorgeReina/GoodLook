64edfe0030f2f547423d554504fd488ec03f2dda
Jorge Reina <reinaromerojorge@gmail.com>
2024-06-14 21:01:08 +0200

    Css and responsive

--- BackEndGoodLook/obj/Debug/net8.0/BackEndGoodLook.AssemblyInfo.cs
+++ BackEndGoodLook/obj/Debug/net8.0/BackEndGoodLook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEndGoodLook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0eb927fbc6ac169c3b1d934e3e10bda3ac965c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cbd66da32bfd9ed994723608d1678d9f270ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEndGoodLook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEndGoodLook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
